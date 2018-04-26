ea0c39e9b9960a34e18c0e9ac5ee425dc8215bac
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-04-26 16:37:05 -0500

    Got modal to work and input to push

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,23 @@
 // Array to store Friend Finder data
 
-var friendData = [];
+var friendData = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
 
 
 module.exports = friendData;
